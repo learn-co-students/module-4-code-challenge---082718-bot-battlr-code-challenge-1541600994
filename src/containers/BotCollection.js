5a03569a6f6ce12eb293658566e214f7b1b014fb
McKenzie Fiege <mckenziefiege@McKenzies-MacBook-Pro.local>
2018-11-07 10:09:50 -0500

    at checkpoint

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -2,14 +2,14 @@ import React from "react";
 import BotCard from "../components/BotCard";
 
 class BotCollection extends React.Component {
-  //your code here
 
   render(){
+    console.log(this.props)
+    let botCards = this.props.bots.map(bot => <BotCard key={bot.id} bot={bot} addBotToArmy={this.props.addBotToArmy} />)
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
+        {botCards}
     		</div>
   	  </div>
   	);
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,16 +1,43 @@
 import React from "react";
+import BotCollection from './BotCollection.js'
+import YourBotArmy from './YourBotArmy.js'
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+  state = {
+    bots: [],
+    armyBots: []
+  }
+
+  componentDidMount(){
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+      .then(resp => resp.json())
+      .then(data => this.setState({
+        bots: data
+      }))
+  }
+
+  addBotToArmy = (bot) => {
+    let botId = bot.id
+    if (this.state.armyBots.includes(bot)){
+      let newArray = this.state.armyBots.filter(bot => bot.id !== botId)
+      this.setState({
+        armyBots: newArray
+      })
+    } else {
+      let newArray = [...this.state.armyBots, bot]
+        this.setState({
+          armyBots: newArray
+        })
+      }
+  }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy bots={this.state.armyBots} />
+        <BotCollection bots={this.state.bots} addBotToArmy={this.addBotToArmy}/>
       </div>
     );
   }
-
 }
-
 export default BotsPage;
